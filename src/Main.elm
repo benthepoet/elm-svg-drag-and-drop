f78f8374b4058117c639b4adcb52bfbf941815a0
Ben Hanna <benpaulhanna.com>
2019-09-06 13:26:11 -0400

    Added events for mouse up and mouse down.

--- src/Main.elm
+++ src/Main.elm
@@ -3,14 +3,20 @@ module Main exposing (..)
 import Browser
 import Html exposing (Html)
 import Html.Attributes
+import Json.Decode
 import Svg exposing (Svg)
 import Svg.Attributes 
-
+import Svg.Events
 
 type alias Circle =
     { cx : Int
     , cy : Int
     , r : Int
+    }
+
+type alias Coordinate =
+    { x: Int
+    , y: Int
     }
 
 type alias Flags =
@@ -19,20 +25,40 @@ type alias Flags =
     }
 
 type alias Model = 
-    { drag : Maybe ( Int, Int )
+    { cursor : Maybe Coordinate
     , circle : Circle
     , width: Int
     , height: Int
     }
 
-type Msg = NoOp
+type Msg 
+    = MouseDown Coordinate
+    | MouseUp
+
+
+coordinateDecoder : Json.Decode.Decoder Coordinate
+coordinateDecoder =
+    Json.Decode.map2 Coordinate
+        (Json.Decode.field "clientX" Json.Decode.int)
+        (Json.Decode.field "clientY" Json.Decode.int)
+
+
+onMouseDown : ( Coordinate -> Msg ) -> Svg.Attribute Msg
+onMouseDown tagger =
+    Svg.Events.on "mousedown" 
+        <| Json.Decode.map tagger coordinateDecoder  
+
+
+onMouseUp : Msg -> Svg.Attribute Msg 
+onMouseUp = 
+    Svg.Events.onMouseUp
 
 
 init : Flags -> ( Model, Cmd Msg )
 init { width, height } = 
     let
         model =
-            { drag = Nothing
+            { cursor = Nothing
             , circle = Circle ( width // 2 ) ( height // 2 ) ( width // 10 )
             , width = width
             , height = height
@@ -44,8 +70,15 @@ init { width, height } =
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        NoOp ->
-            ( model, Cmd.none ) 
+        MouseDown coordinate ->
+            ( { model | cursor = Just coordinate }
+            , Cmd.none
+            )
+
+        MouseUp ->
+            ( { model | cursor = Nothing }
+            , Cmd.none
+            )
 
 
 view : Model -> Html Msg
@@ -58,6 +91,12 @@ view model =
             [ Svg.svg
                 [ Svg.Attributes.width <| String.fromInt model.width
                 , Svg.Attributes.height <| String.fromInt model.height
+                , case model.cursor of
+                    Nothing ->
+                        Html.Attributes.attribute "data-inactive" "true"
+
+                    Just _ ->
+                        onMouseUp MouseUp
                 ]
                 [ Svg.circle
                     [ Svg.Attributes.fill "yellow"
@@ -65,7 +104,9 @@ view model =
                     , Svg.Attributes.strokeWidth "4"
                     , Svg.Attributes.cx <| String.fromInt model.circle.cx
                     , Svg.Attributes.cy <| String.fromInt model.circle.cy
-                    , Svg.Attributes.r <| String.fromInt model.circle.r ]
+                    , Svg.Attributes.r <| String.fromInt model.circle.r
+                    , onMouseDown MouseDown
+                    ]
                     []
                 ]
             ]
